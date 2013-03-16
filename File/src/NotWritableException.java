915b82df2b1a45b0164a883b288476dc9521667d
Mathias Benoit <mathiasbenoit@hotmail.com>
2013-03-16 12:04:52 +0100

    initial commit

---
+++ File/src/NotWritableException.java
@@ -0,0 +1,11 @@
+
+public class NotWritableException extends Exception {
+
+	
+	private static final long serialVersionUID = 7422344796018755413L;
+
+	public NotWritableException(String msg){
+		super(msg);
+	}
+	
+}
