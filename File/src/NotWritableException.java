810fb37d2da9e2584298b477bba7380120df869b
isostorm <mathiasbenoit@hotmail.com>
2013-03-22 12:21:53 +0100

    Update NotWritableException.java

--- File/src/NotWritableException.java
+++ File/src/NotWritableException.java
@@ -1,11 +1,13 @@
-
 public class NotWritableException extends RuntimeException {
 
 	
 	private static final long serialVersionUID = 7422344796018755413L;
-
+	
+	private DiskItem diskItem;
+	
 	public NotWritableException(DiskItem item){
 		super("The item \""+item.getName() +"\" is not writable");
+		diskItem = item;
 	}
 	
 }
